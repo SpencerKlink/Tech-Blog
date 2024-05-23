8f715e5dc434f011b266ec27799b08368e554bb8
Spencer Klinkowize <spencerk@Spencers-MacBook-Pro-M3.local>
2024-05-22 20:53:52 -0400

    server.js setup

---
+++ Desktop/tech-blog/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const sessions = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(sessions.Store);
+
+const sequelize = require('./config/config');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+    resave: false,
+    saveUninitialized: true,
+    rolling: true
+};
+
+app.use(sessions(sess));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
